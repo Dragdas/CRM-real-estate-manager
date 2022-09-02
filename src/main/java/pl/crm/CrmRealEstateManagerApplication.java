ba4a1ef72a70f5a1360c90a2fadca08fa9d5136d
PLecicki <p.lecicki@gmail.com>
2022-09-02 16:12:53 +0200

    first commit

---
+++ src/main/java/pl/crm/CrmRealEstateManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRealEstateManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmRealEstateManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/crm/CrmRealEstateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmRealEstateManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
