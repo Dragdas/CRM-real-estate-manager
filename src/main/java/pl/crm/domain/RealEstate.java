597410e45aecdb70ccd83b99641c99129360b388
humptydumpty <mateusz.wojtas89@gmail.com>
2022-09-07 13:01:09 +0200

    Entities created

---
+++ src/main/java/pl/crm/domain/RealEstate.java
@@ -0,0 +1,27 @@
+package pl.crm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "real_estate")
+public class RealEstate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "real_estate_id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @OneToMany(targetEntity = Premis.class, mappedBy = "realEstate")
+    private List<Premis> premises;
+
+}
---
+++ src/main/java/pl/crm/domain/Tenant.java
@@ -0,0 +1,33 @@
+package pl.crm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "tenant")
+public class Tenant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tenant_id")
+    private long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "user_password")
+    private String userPassword;
+
+    @OneToMany(targetEntity = Invoice.class, mappedBy = "recipient")
+    private List<Invoice> invoices;
+
+    @OneToMany(targetEntity = Premis.class, mappedBy = "tenant")
+    private List<Premis> premises;
+
+}
